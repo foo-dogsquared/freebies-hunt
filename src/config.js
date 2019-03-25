97add78abe3bda269ae54abfb7071d91ac6b6db0
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-03-25 23:38:48 +0800

    Create a unified config file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: `Freebies Hunt`,
+  description: `It's a personal list of digital freebies that can be found on the web.`,
+  googleAnalyticsId: `UA-126718538-4`,
+  author: `foo-dogsquared`,
+  socialLinks: {
+    github: `https://github.com/foo-dogsquared/`,
+    twitter: `https://twitter.com/foo_dogsquared`,
+    email: `mailto:foo.dogsquared@gmail.com`
+  },
+  repoLink: `https://github.com/foo-dogsquared/freebies-hunt`,
+  apiRepoLink: `https://github.com/foo-dogsquared/freebies-hunt-api`,
+}
