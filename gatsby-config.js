b4c05cc56355e7dd1f69182aeaa206a8ea22f60b
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-04-24 17:56:57 +0800

    Support offline usage

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,7 @@ module.exports = {
         head: true,
         anonymize: true,
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    },
+    'gatsby-plugin-offline',
   ],
 }
