a9169e994b225879d58cea52a6aebcbdd4942652
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-04-24 17:56:22 +0800

    Show the categories by hue

--- src/components/categoryGrid.js
+++ src/components/categoryGrid.js
@@ -5,6 +5,26 @@ import color from "color"
 import "./categoryGrid.scss"
 
 const categoryGrid = ({ categories }) => {
+  // TODO:
+  // Make the categories to be an array
+  // Sort them by their `mainColor`
+  // Render the cards with the sorted by color list
+  // PROFIT!
+
+  categories.sort((current, next) => {
+    // color instances
+    const currentCategoryColor = color(current.mainColor);
+    const nextCategoryColor = color(next.mainColor);
+
+    if (currentCategoryColor.hue() > nextCategoryColor.hue()) {
+      return 1;
+    } else if (currentCategoryColor.hue() < nextCategoryColor.hue()) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <section className="categories-grid">
       {categories.map((category) => {
