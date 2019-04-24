12d1fa58c3ca5d97ac3e70655705b44a78219df0
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-04-24 17:54:40 +0800

    Refactor and create component for freebie list

---
+++ src/components/freebieList.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+// npm scripts
+import marked from "marked"
+
+// components
+import ExternalLink from "./externalLink"
+
+// CSS stylings
+import "./freebieList.scss"
+
+const FreebieList = ({ freebies }) => {
+  return (
+    <ul className="freebies">
+      {freebies.map(item => {
+        let description = null;
+        let personalComment = null;
+
+        if (item.description) {
+          description = <p className="freebie-description" dangerouslySetInnerHTML={{__html: marked(item.description)}}></p>;
+        }
+
+        if (item.personalComment) {
+          personalComment = <small className="personal-comment" dangerouslySetInnerHTML={{__html: marked(item.personalComment)}}></small>
+        }
+
+        return (
+          <li className="freebie-item" key={item.name}>
+            <h2><ExternalLink href={item.url} dangerouslySetInnerHTML={{__html: marked(item.name)}} /></h2>
+            {description}
+            {personalComment}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default FreebieList;
