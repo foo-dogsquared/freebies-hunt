fb905f2d3d2584c904c35b1605d92c997fafb2de
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-04-24 17:56:00 +0800

    Update layout code

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,14 +26,7 @@ const Layout = ({ children, color }) => (
     <Header siteTitle={config.title} />
     <div className="elliptical-border"
       style={generateStyleObject(color)}>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-        >
+      <div className="content-wrapper">
         <main>{children}</main>
         <section className="about">
           <h2>What is Freebies Hunt?</h2>
